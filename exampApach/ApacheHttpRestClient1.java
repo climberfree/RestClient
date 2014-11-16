b22007a2c0e17cda9f910b9e100af71ccf7bf5dd
climberfree <yulia.frishko@gmail.com>
2014-11-17 00:48:39 +0200

    Update ApacheHttpRestClient1.java

--- exampApach/ApacheHttpRestClient1.java
+++ exampApach/ApacheHttpRestClient1.java
@@ -16,34 +16,38 @@ import org.json.JSONObject;
  */
 public class ApacheHttpRestClient1 {
 
+    private DefaultHttpClient httpclient = new DefaultHttpClient();
+    // specify the host, protocol, and port
+    HttpHost target = new HttpHost("query.yahooapis.com", 80, "http");
+
     public static void main(String[] args) {
-        DefaultHttpClient httpclient = new DefaultHttpClient();
+        ApacheHttpRestClient1 apacheHttpRestClient1=new ApacheHttpRestClient1();
+        apacheHttpRestClient1.getRate("USDRUB");
+    }
+
+    private double getRate(String currenceName)
+    {
+        double rate = 0;
         try {
-            // specify the host, protocol, and port
-            HttpHost target = new HttpHost("query.yahooapis.com", 80, "http");
 
-            // specify the get request
-            HttpGet getRequest = new HttpGet("/v1/public/yql?q=select%20rate%2Cname%20from%20csv%20where%20url%3D'http%3A%2F%2Fdownload.finance.yahoo.com%2Fd%2Fquotes%3Fs%3DUSDCHF%253DX%26f%3Dl1n'%20and%20columns%3D'rate%2Cname'&format=json");
-
-            System.out.println("executing request to " + target);
+            HttpGet getRequest = new HttpGet
+                    ("/v1/public/yql?q=select%20rate%2Cname%20from%20csv%20where%20url%3D'http%3A%2F%2Fdownload.finance.yahoo.com%2Fd%2Fquotes%3Fs%3D"+currenceName+"%253DX%26f%3Dl1n'%20and%20columns%3D'rate%2Cname'&format=json");
 
             HttpResponse httpResponse = httpclient.execute(target, getRequest);
             HttpEntity entity = httpResponse.getEntity();
-
-
-
             System.out.println("----------------------------------------");
             System.out.println(httpResponse.getStatusLine());
-            Header[] headers = httpResponse.getAllHeaders();
-            for (int i = 0; i < headers.length; i++) {
-                System.out.println(headers[i]);
-            }
-            System.out.println("----------------------------------------");
 
             if (entity != null) {
                 JSONObject jsonObj = new JSONObject(EntityUtils.toString(entity));
-                System.out.println("query = > "+jsonObj.get("query"));
+                JSONObject query= (JSONObject) jsonObj.get("query");
+                JSONObject result= (JSONObject) query.get("results");
+                JSONObject row= (JSONObject) result.get("row");
+                rate= Double.parseDouble((String) row.get("rate"));
+
+                System.out.println("ndgkjb "+rate);
                 System.out.println("query " + jsonObj.toString());
+
             }
 
         } catch (Exception e) {
@@ -54,5 +58,6 @@ public class ApacheHttpRestClient1 {
             // immediate deallocation of all system resources
             httpclient.getConnectionManager().shutdown();
         }
+        return rate;
     }
 }
